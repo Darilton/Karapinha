5d9dda8af983b09bc4b60ccd6246b881e26b964e
gumball <dariltonmario14@gmail.com>
2024-07-03 16:43:11 +0100

    Initialize project and create database with category and service models

---
+++ src/server/Data/AppDbConetxt.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Data;
+
+public class AppDbCotnext: DbContext
+{
+    public AppDbCotnext(DbContextOptions<AppDbCotnext> options) : base(options) { }
+    public DbSet<Category> Categories{ get; set; }
+    public DbSet<Service> Services{ get; set; }
+}
---
+++ src/server/Model/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Model;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public ICollection<Service>? Services { get; set; }
+}
